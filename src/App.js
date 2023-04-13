520b93fb0b5a8c1c6a45a8a639b369de4bf4cf94
Raffaele Crocco <raffaelecrocco2000@gmail.com>
2023-04-13 13:34:35 +0200

    basic ui setup

---
+++ src/App.js
@@ -0,0 +1,244 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-[#1E1E1E]">
+      <div className="p-3 w-max-full h-min-[75vh] md:h-[75vh]">
+        <div className="bg-[#E3E3E3] rounded-[21px] block md:flex md:rounded-[41px] !rounded-br-[0px] h-full w-max-full p-10">
+          <div className="w-max-full md:w-1/2 flex flex-col justify-between">
+            <img src="titolo.svg" />
+            <div class="text-sm font-extrabold text-gray-700">
+              <p>GITHUB</p>
+              <p>LINKEDIN</p>
+              <p>TELEGRAM</p>
+              <p className="mt-5">info@raffaelecrocco.com</p>
+              <p>+39 3661055047</p>
+            </div>
+          </div>
+          <div className="w-max-full relative md:w-1/2 flex h-full justify-center">
+            <div className="w-1/2 h-fit mt-10">
+              <p className="text-3xl font-bold tracking-tight text-gray-900">
+                About me
+                <p className="text-sm mt-1 tracking-tight text-gray-500">
+                  // marzo 2022 - progetto personale
+                </p>
+              </p>
+              <p class="text-sm mt-5 text-gray-700">
+                Laureando in Ingegneria Informatica all'Università degli Studi
+                di Trento. Questo sito e i progetti al suo interno parlano di me
+                e del mio viaggio nel mondo della programmazione che ho sempre
+                percepito come il modo più veloce per rendere reali le nostre
+                idee. Al momento scrivo la tesi e lavoro per Onlytech Industries
+                e ai loro progetti in smart working. Sono tutta via disponibile
+                a collaborazioni Co. Co. Co. e a proposte di progetto di vario
+                tipo. Per qualsiasi informazione Contattami o scarica il mio
+                Curriculum
+              </p>
+            </div>
+            <img src="smile.png" className="absolute -bottom-10 h-1/3" />
+          </div>
+        </div>
+      </div>
+      <div className="h-min-screen md:h-screen block md:flex">
+        <div className="w-max-full md:w-1/3">
+          <div className="w-max-full h-1/4 p-3">
+            <div className="bg-[#E3E3E3] flex flex-col justify-between rounded-[21px] md:rounded-[31px] !rounded-tr-[0px] h-full w-max-full p-7">
+              <div className="flex justify-between">
+                <p className="text-3xl font-bold tracking-tight -mt-2 text-gray-900">
+                  Help-ukranians.org
+                  <p className="text-sm mt-1 tracking-tight text-gray-500">
+                    // marzo 2022 - progetto personale
+                  </p>
+                </p>
+                <img src="arrow.svg" />
+              </div>
+              <p class="text-sm text-gray-700">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order. Here are the biggest
+                enterprise technology acquisitions of 2021 so far, in reverse
+                chronological order. Here are the biggest enterprise technology
+                acquisitions of 2021 so far, in reverse chronological order.
+              </p>
+            </div>
+          </div>
+          <div className="w-max-full h-3/4 p-3">
+            <div className="h-full w-max-full p-10 flex h-full justify-center text-[#E3E3E3]">
+              <div className="w-1/2 h-fit my-auto">
+                <p className="text-3xl font-bold tracking-tight">
+                  Altro da dire
+                  <p className="text-sm mt-1 tracking-tight text-gray-400">
+                    // marzo 2022 - progetto personale
+                  </p>
+                </p>
+                <p class="text-sm mt-5">
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order. Here are the
+                  biggest enterprise technology acquisitions of 2021 so far, in
+                  reverse chronological order.
+                </p>
+                <p class="text-sm mt-10">
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order. Here are the
+                  biggest enterprise technology acquisitions of 2021 so far, in
+                  reverse chronological order.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-max-full md:w-1/3">
+          <div className="w-max-full h-1/4 p-3">
+            <div className="bg-[#E3E3E3] flex flex-col justify-between rounded-[21px] md:rounded-[31px] !rounded-tl-[0px] h-full w-max-full p-7">
+              <div className="flex justify-between">
+                <p className="text-3xl font-bold tracking-tight -mt-2 text-gray-900">
+                  Help-ukranians.org
+                  <p className="text-sm mt-1 tracking-tight text-gray-500">
+                    // marzo 2022 - progetto personale
+                  </p>
+                </p>
+                <img src="arrow.svg" />
+              </div>
+              <p class="text-sm text-gray-700">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order. Here are the biggest
+                enterprise technology acquisitions of 2021 so far, in reverse
+                chronological order. Here are the biggest enterprise technology
+                acquisitions of 2021 so far, in reverse chronological order.
+              </p>
+            </div>
+          </div>
+          <div className="w-max-full h-1/4 p-3">
+            <div className="border border-4 border-[#E3E3E3] text-[#E3E3E3] flex flex-col justify-between rounded-[21px] md:rounded-[31px] !rounded-bl-[0px] h-full w-max-full p-7">
+              <div className="flex justify-between">
+                <p className="text-3xl font-bold tracking-tight -mt-2">
+                  Help-ukranians.orgs
+                  <p className="text-sm mt-1 tracking-tight text-gray-400">
+                    // marzo 2022 - progetto personale
+                  </p>
+                </p>
+                <img src="arrow2.svg" />
+              </div>
+              <p class="text-sm">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order. Here are the biggest
+                enterprise technology acquisitions of 2021 so far, in reverse
+                chronological order. Here are the biggest enterprise technology
+                acquisitions of 2021 so far, in reverse chronological order.
+              </p>
+            </div>
+          </div>
+          <div className="w-max-full h-1/4 p-3">
+            <div className="bg-[#E3E3E3] flex flex-col justify-between rounded-[21px] md:rounded-[31px] !rounded-br-[0px] h-full w-max-full p-7">
+              <div className="flex justify-between">
+                <p className="text-3xl font-bold tracking-tight -mt-2 text-gray-900">
+                  Help-ukranians.org
+                  <p className="text-sm mt-1 tracking-tight text-gray-500">
+                    // marzo 2022 - progetto personale
+                  </p>
+                </p>
+                <img src="arrow.svg" />
+              </div>
+              <p class="text-sm text-gray-700">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order. Here are the biggest
+                enterprise technology acquisitions of 2021 so far, in reverse
+                chronological order. Here are the biggest enterprise technology
+                acquisitions of 2021 so far, in reverse chronological order.
+              </p>
+            </div>
+          </div>
+          <div className="w-max-full h-1/4 p-3">
+            <div className="bg-[#E3E3E3] flex flex-col justify-between rounded-[21px] md:rounded-[31px] !rounded-tr-[0px] h-full w-max-full p-7">
+              <div className="flex justify-between">
+                <p className="text-3xl font-bold tracking-tight -mt-2 text-gray-900">
+                  Help-ukranians.org
+                  <p className="text-sm mt-1 tracking-tight text-gray-500">
+                    // marzo 2022 - progetto personale
+                  </p>
+                </p>
+                <img src="arrow.svg" />
+              </div>
+              <p class="text-sm text-gray-700">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order. Here are the biggest
+                enterprise technology acquisitions of 2021 so far, in reverse
+                chronological order. Here are the biggest enterprise technology
+                acquisitions of 2021 so far, in reverse chronological order.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="w-max-full md:w-1/3">
+          <div className="w-max-full h-full p-3">
+            <div className="border border-4 border-[#E3E3E3] text-[#E3E3E3] rounded-[21px] md:rounded-[41px] !rounded-tr-[0px] h-full w-max-full p-10">
+              <p className="text-3xl font-bold tracking-tight -mt-2">
+                Shotout del mese
+                <p className="text-sm mt-1 tracking-tight text-gray-400">
+                  // marzo 2022 - progetto personale
+                </p>
+              </p>
+              <div className="flex justify-between mt-10">
+                <p class="text-sm">
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order. Here are the
+                  biggest enterprise technology acquisitions of 2021 so far, in
+                  reverse chronological order.
+                </p>
+                <img src="arrow.svg" />
+              </div>
+              <div className="flex justify-between mt-10">
+                <p class="text-sm">
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order. Here are the
+                  biggest enterprise technology acquisitions of 2021 so far, in
+                  reverse chronological order.
+                </p>
+                <img src="arrow.svg" />
+              </div>
+              <div className="flex justify-between mt-10">
+                <p class="text-sm">
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order. Here are the
+                  biggest enterprise technology acquisitions of 2021 so far, in
+                  reverse chronological order.
+                </p>
+                <img src="arrow.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-3 w-max-full h-min-[25vh] md:h-[25vh]">
+        <div className="border border-4 border-[#E3E3E3] text-[#E3E3E3] rounded-[21px] block md:flex md:rounded-[41px] !rounded-br-[0px] h-full w-max-full block md:flex space-x-10 p-10">
+          <div className="w-1/4 h-fit my-auto">
+            <p className="text-sm tracking-tight text-gray-400">
+              // marzo 2022 - progetto personale
+            </p>
+            <p class="text-sm mt-2">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order. Here are the biggest
+              enterprise technology acquisitions of 2021 so far.
+            </p>
+          </div>
+          <div className="w-1/4 h-fit my-auto"></div>
+          <div className="w-1/4 h-fit my-auto md:flex justify-end">
+            <p className="text-sm tracking-tight">
+              marzo 2022 - progetto personale
+            </p>
+          </div>
+          <div className="w-1/4 md:flex justify-center h-fit my-auto">
+            <div class="text-sm text-end font-extrabold">
+              <p>GITHUB</p>
+              <p>LINKEDIN</p>
+              <p>TELEGRAM</p>
+              <p className="mt-5">info@raffaelecrocco.com</p>
+              <p>+39 3661055047</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
