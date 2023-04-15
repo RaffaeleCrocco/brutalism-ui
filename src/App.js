7e62484ddaacd6ee4ea9fc8871d90bd5e9cef7d8
Raffaele Crocco <raffaelecrocco2000@gmail.com>
2023-04-15 18:13:55 +0200

    Update testi e ui

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
       <div className="p-3 w-max-full h-min-[75vh] md:h-[75vh]">
         <div className="bg-[#E3E3E3] rounded-[21px] block md:flex md:rounded-[41px] !rounded-br-[0px] h-full w-max-full p-10">
           <div className="w-max-full md:w-1/2 flex flex-col justify-between">
-            <img src="titolo.svg" />
+            <div>
+              <img src="titolo.svg" />
+              <p className="font-extrabold mt-6">
+                Attualmente disponibile per progetti e Co. Co. Co.
+              </p>
+            </div>
             <div class="text-sm font-extrabold text-gray-700">
               <p>GITHUB</p>
               <p>LINKEDIN</p>
@@ -18,21 +23,18 @@ function App() {
           <div className="w-max-full relative md:w-1/2 flex h-full justify-center">
             <div className="w-1/2 h-fit mt-10">
               <p className="text-3xl font-bold tracking-tight text-gray-900">
-                About me
                 <p className="text-sm mt-1 tracking-tight text-gray-500">
-                  // marzo 2022 - progetto personale
+                  // aprile 2023 - Raffaele Crocco
                 </p>
               </p>
               <p class="text-sm mt-5 text-gray-700">
-                Laureando in Ingegneria Informatica all'Università degli Studi
-                di Trento. Questo sito e i progetti al suo interno parlano di me
-                e del mio viaggio nel mondo della programmazione che ho sempre
-                percepito come il modo più veloce per rendere reali le nostre
-                idee. Al momento scrivo la tesi e lavoro per Onlytech Industries
-                e ai loro progetti in smart working. Sono tutta via disponibile
-                a collaborazioni Co. Co. Co. e a proposte di progetto di vario
-                tipo. Per qualsiasi informazione Contattami o scarica il mio
-                Curriculum
+                Laureato in Ingegneria Informatica all'Università degli Studi di
+                Trento. Questo sito e i progetti al suo interno dimostrano la
+                mia percezione verso la programmazione come la realizzazione
+                concreta d'idee. Al momento lavoro per Onlytech Industries e ai
+                loro progetti in smart working. Sono tutta via disponibile a
+                collaborazioni Co. Co. Co. e a proposte di progetto di vario
+                tipo. Per qualsiasi informazione contattami.
               </p>
             </div>
             <img src="smile.png" className="absolute -bottom-10 h-1/3" />
@@ -43,21 +45,17 @@ function App() {
         <div className="w-max-full md:w-1/3">
           <div className="w-max-full h-1/4 p-3">
             <div className="bg-[#E3E3E3] flex flex-col justify-between rounded-[21px] md:rounded-[31px] !rounded-tr-[0px] h-full w-max-full p-7">
-              <div className="flex justify-between">
-                <p className="text-3xl font-bold tracking-tight -mt-2 text-gray-900">
-                  Help-ukranians.org
-                  <p className="text-sm mt-1 tracking-tight text-gray-500">
-                    // marzo 2022 - progetto personale
-                  </p>
+              <p className="text-3xl font-bold tracking-tight -mt-2 text-gray-900">
+                https://www.help-ukrainians.org/
+                <p className="text-sm mt-1 tracking-tight text-gray-500">
+                  // marzo 2022 - progetto personale
                 </p>
-                <img src="arrow.svg" />
-              </div>
+              </p>
               <p class="text-sm text-gray-700">
-                Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order. Here are the biggest
-                enterprise technology acquisitions of 2021 so far, in reverse
-                chronological order. Here are the biggest enterprise technology
-                acquisitions of 2021 so far, in reverse chronological order.
+                E' un servizio gratuito a disposizione dei cittadini che
+                permette la condivisione di iniziative benefiche a supporto del
+                popolo ucraino. Condividiamo informazioni utili per il
+                volontariato attraverso enti verificati.
               </p>
             </div>
           </div>
@@ -67,20 +65,20 @@ function App() {
                 <p className="text-3xl font-bold tracking-tight">
                   Altro da dire
                   <p className="text-sm mt-1 tracking-tight text-gray-400">
-                    // marzo 2022 - progetto personale
+                    // softskill e commenti
                   </p>
                 </p>
                 <p class="text-sm mt-5">
-                  Here are the biggest enterprise technology acquisitions of
-                  2021 so far, in reverse chronological order. Here are the
-                  biggest enterprise technology acquisitions of 2021 so far, in
-                  reverse chronological order.
+                  Comunicare e coordinare sono arti che mi affascinano da
+                  sempre. E' nella mia routine lo studio di entrambe le
+                  discipline spinto da una forte passione per esse.
                 </p>
                 <p class="text-sm mt-10">
-                  Here are the biggest enterprise technology acquisitions of
-                  2021 so far, in reverse chronological order. Here are the
-                  biggest enterprise technology acquisitions of 2021 so far, in
-                  reverse chronological order.
+                  Il tool che preferisco per la progettazione grafica è
+                  solitamente Figma. La maggior parte dei siti che realizzo
+                  sfruttano Typescript / React come framework javascript. Il
+                  sito che stai visitando segue una linea di design detta
+                  brutalismo, trend 2023.
                 </p>
               </div>
             </div>
@@ -89,81 +87,66 @@ function App() {
         <div className="w-max-full md:w-1/3">
           <div className="w-max-full h-1/4 p-3">
             <div className="bg-[#E3E3E3] flex flex-col justify-between rounded-[21px] md:rounded-[31px] !rounded-tl-[0px] h-full w-max-full p-7">
-              <div className="flex justify-between">
-                <p className="text-3xl font-bold tracking-tight -mt-2 text-gray-900">
-                  Help-ukranians.org
-                  <p className="text-sm mt-1 tracking-tight text-gray-500">
-                    // marzo 2022 - progetto personale
-                  </p>
+              <p className="text-3xl font-bold tracking-tight -mt-2 text-gray-900">
+                https://www.onlytechindustries.com/
+                <p className="text-sm mt-1 tracking-tight text-gray-500">
+                  // gennaio 2023 - progetto lavorativo
                 </p>
-                <img src="arrow.svg" />
-              </div>
+              </p>
               <p class="text-sm text-gray-700">
-                Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order. Here are the biggest
-                enterprise technology acquisitions of 2021 so far, in reverse
-                chronological order. Here are the biggest enterprise technology
-                acquisitions of 2021 so far, in reverse chronological order.
+                Azienda italiana con esperienza in Blockchain pubbliche e
+                private che attraverso l’utilizzo di un ampio stack tecnologico,
+                offre consulenza progettuale, sviluppo e integrazione di
+                soluzioni Blockchain, sicurezza, e scalabilità.
               </p>
             </div>
           </div>
           <div className="w-max-full h-1/4 p-3">
             <div className="border border-4 border-[#E3E3E3] text-[#E3E3E3] flex flex-col justify-between rounded-[21px] md:rounded-[31px] !rounded-bl-[0px] h-full w-max-full p-7">
-              <div className="flex justify-between">
-                <p className="text-3xl font-bold tracking-tight -mt-2">
-                  Help-ukranians.orgs
-                  <p className="text-sm mt-1 tracking-tight text-gray-400">
-                    // marzo 2022 - progetto personale
-                  </p>
+              <p className="text-3xl font-bold tracking-tight -mt-2">
+                https://www.vendimpresa.it/
+                <p className="text-sm mt-1 tracking-tight text-gray-400">
+                  // febbraio 2023 - progetto lavorativo
                 </p>
-                <img src="arrow2.svg" />
-              </div>
+              </p>
+
               <p class="text-sm">
-                Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order. Here are the biggest
-                enterprise technology acquisitions of 2021 so far, in reverse
-                chronological order. Here are the biggest enterprise technology
-                acquisitions of 2021 so far, in reverse chronological order.
+                Landing che anticipa il portale attualmente sotto sviluppo
+                VendImpresa, una realta immobiliare innovativa per la
+                compravendita di asset aziendali composto da un gruppo di
+                professionisti del settore.
               </p>
             </div>
           </div>
           <div className="w-max-full h-1/4 p-3">
             <div className="bg-[#E3E3E3] flex flex-col justify-between rounded-[21px] md:rounded-[31px] !rounded-br-[0px] h-full w-max-full p-7">
-              <div className="flex justify-between">
-                <p className="text-3xl font-bold tracking-tight -mt-2 text-gray-900">
-                  Help-ukranians.org
-                  <p className="text-sm mt-1 tracking-tight text-gray-500">
-                    // marzo 2022 - progetto personale
-                  </p>
+              <p className="text-3xl font-bold tracking-tight -mt-2 text-gray-900">
+                https://ns-training.vercel.app/
+                <p className="text-sm mt-1 tracking-tight text-gray-500">
+                  // gennaio 2022 - progetto personale
                 </p>
-                <img src="arrow.svg" />
-              </div>
+              </p>
               <p class="text-sm text-gray-700">
-                Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order. Here are the biggest
-                enterprise technology acquisitions of 2021 so far, in reverse
-                chronological order. Here are the biggest enterprise technology
-                acquisitions of 2021 so far, in reverse chronological order.
+                Tool interattivo per gli studenti che vogliono allenare la
+                propria conoscenza sul tema della Network Security in
+                preparazione all'esame. Il sito e il database relativo sono da
+                usare solo a scopo educativo.
               </p>
             </div>
           </div>
           <div className="w-max-full h-1/4 p-3">
             <div className="bg-[#E3E3E3] flex flex-col justify-between rounded-[21px] md:rounded-[31px] !rounded-tr-[0px] h-full w-max-full p-7">
-              <div className="flex justify-between">
-                <p className="text-3xl font-bold tracking-tight -mt-2 text-gray-900">
-                  Help-ukranians.org
-                  <p className="text-sm mt-1 tracking-tight text-gray-500">
-                    // marzo 2022 - progetto personale
-                  </p>
+              <p className="text-3xl font-bold tracking-tight -mt-2 text-gray-900">
+                /scripts/vision.py
+                <p className="text-sm mt-1 tracking-tight text-gray-500">
+                  // dicembre 2021 - progetto personale
                 </p>
-                <img src="arrow.svg" />
-              </div>
+              </p>
               <p class="text-sm text-gray-700">
-                Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order. Here are the biggest
-                enterprise technology acquisitions of 2021 so far, in reverse
-                chronological order. Here are the biggest enterprise technology
-                acquisitions of 2021 so far, in reverse chronological order.
+                Codice python per un algoritmo di computer vision creato per
+                riconoscere e classificare lego in un ambiente simulato e
+                permettere ad un braccio robotico controllato con ROS di
+                manipolarli.
               </p>
             </div>
           </div>
@@ -172,9 +155,10 @@ function App() {
           <div className="w-max-full h-full p-3">
             <div className="border border-4 border-[#E3E3E3] text-[#E3E3E3] rounded-[21px] md:rounded-[41px] !rounded-tr-[0px] h-full w-max-full p-10">
               <p className="text-3xl font-bold tracking-tight -mt-2">
-                Shotout del mese
+                Shoutout del mese
                 <p className="text-sm mt-1 tracking-tight text-gray-400">
-                  // marzo 2022 - progetto personale
+                  // i progetti e le persone che hanno influenzato di più il mio
+                  mese.
                 </p>
               </p>
               <div className="flex justify-between mt-10">
